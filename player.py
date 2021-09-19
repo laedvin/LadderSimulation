688b46e1a88851982013253e7f7ed84a2fe2c6e1
laedvin <edvin.lam.el@gmail.com>
2021-09-19 20:41:14 +0200

    Added a more complex skill representation.

--- player.py
+++ player.py
@@ -1,6 +1,11 @@
 class Player:
-    def __init__(self, skill=100):
-        self.skill = skill  # Actual skill
-        self.mmr = 1000  # Just a number
+    def __init__(self, skill_x=0, skill_y=0, skill_z=0):
+        # Define some abstract skills
+        self.skill_x = skill_x
+        self.skill_y = skill_y
+        self.skill_z = skill_z
+
+        # Define ranking statistics
+        self.mmr = 1000
         self.wins = 0
         self.losses = 0
--- simulation.py
+++ simulation.py
@@ -1,25 +1,54 @@
 from player import Player
 import numpy as np
 import random
+import math
 
 
 # Initialize stuff
 num_players = 1000
-num_games = 100000  # Number of games to simulate
+num_games = 200000  # Number of games to simulate
 k = 30  # the K-factor, pertaining to how much MMR adjusts after a game
+playstyle_factor = 1  # How different skills affect winrate in a matchup
+winrate_factor = 20  # The adjusted skill difference needed for ~91% winrate
+# playstyle_factor = 0 means taking the sum of a player's x, y and z.
 
-# Note that the skill distribution might not be good.
-players = [Player(skill) for skill in np.random.normal(0, 1, num_players)]
+# Note that the skill distribution isn't tuned.
+xs = np.abs(np.random.normal(0, 1, num_players))
+ys = np.abs(np.random.normal(0, 1, num_players))
+zs = np.abs(np.random.normal(0, 1, num_players))
+skills = zip(xs, ys, zs)
+players = [Player(x, y, z) for x, y, z in skills]
+
+
+def calc_winrate(player_a, player_b):
+    """
+    Takes two player objects as inputs.
+    Returns the probability of winning for player A.
+    """
+
+    # Extract the individual skills
+    a_x, a_y, a_z = player_a.skill_x, player_a.skill_y, player_a.skill_z
+    b_x, b_y, b_z = player_b.skill_x, player_b.skill_y, player_b.skill_z
+
+    # Calculate the adjusted skill
+    skill_a = (a_x * math.exp((a_x-b_y) * playstyle_factor) +
+               a_y * math.exp((a_y-b_z) * playstyle_factor) +
+               a_z * math.exp((a_z-b_x) * playstyle_factor))
+
+    skill_b = (b_x * math.exp((b_x-a_y) * playstyle_factor) +
+               b_y * math.exp((b_y-a_z) * playstyle_factor) +
+               b_z * math.exp((b_z-a_x) * playstyle_factor))
+
+    skill_a = 10**(skill_a/winrate_factor)
+    skill_b = 10**(skill_b/winrate_factor)
+
+    return skill_a/(skill_a + skill_b)
 
 
 # Play some games
 for i in range(num_games):
     player_a, player_b = random.sample(players, 2)
-    # Calculate probability of player A winning,
-    # based on Elo (logistics curve)
-    skill_a = 10**player_a.skill
-    skill_b = 10**player_b.skill
-    p_a_win = skill_a/(skill_a + skill_b)
+    p_a_win = calc_winrate(player_a, player_b)
 
     if random.random() < p_a_win:
         # Player A won
@@ -43,9 +72,20 @@ for i in range(num_games):
     player_a.mmr += k * (s_a - e_a)
     player_b.mmr += k * (s_b - e_b)
 
-for player in players:
-    print(player.mmr)
+#for player in players:
+#    print(player.mmr)
+
+strongest_player = players[np.argmax([player.mmr for player in players])]
+print(f"\nStrongest player, w-l: {strongest_player.wins}-{strongest_player.losses}," +
+      f"\nMMR: {strongest_player.mmr}\nSkill x: {strongest_player.skill_x}" + 
+      f"\nSkill y: {strongest_player.skill_y}\nSkill z: {strongest_player.skill_z}")
+
+weakest_player = players[np.argmax([-player.mmr for player in players])]
+print(f"\nWeakest player, w-l: {weakest_player.wins}-{weakest_player.losses}," +
+      f"\nMMR: {weakest_player.mmr}\nSkill x: {weakest_player.skill_x}" + 
+      f"\nSkill y: {weakest_player.skill_y}\nSkill z: {weakest_player.skill_z}")
 
 sample_player = random.choice(players)
-print(f"Sample win-loss: {sample_player.wins}-{sample_player.losses}," +
-      f"\nMMR: {sample_player.mmr}\nSkill: {sample_player.skill}")
+print(f"\nRandom player, w-l: {sample_player.wins}-{sample_player.losses}," +
+      f"\nMMR: {sample_player.mmr}\nSkill x: {sample_player.skill_x}" + 
+      f"\nSkill y: {sample_player.skill_y}\nSkill z: {sample_player.skill_z}")
